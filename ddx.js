a778882dc4b4452f2df6148e2b562fed6f476a15
clare <justclarifying@gmail.com>
2017-07-05 18:51:27 +0100

    Revert "monotonicity, derivatives"
    This reverts commit cdb3e43d0c78f986f0978a1022e10faf9ce1eccb.
    
    i done goofed

--- ddx.js
+++ ddx.js
@@ -3,13 +3,8 @@
 
 
 function derive(xs, ys){
-	var derivs = [];
-	ys.unshift(ys[0] - (ys[1] - ys[0]));
-	ys.push(ys[ys.length - 1] - (ys[ys.length -1] - ys[ys.length - 2]));
-	// Pad first and last values of our function to make derivs easier
-	for (i = 0; i < xs.length; i++){
-		derivs.push((ys[i+2] - ys[i])/20); 
-	}
-	return derivs;
+	n = xs.length;
+	console.log(n)
 }
 
+derive([0,1], [2,3]);
