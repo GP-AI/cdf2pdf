0303aced715e2633f4a4b7b7e278da9d4afe159d
13bzhang <baobao.zhang@yale.edu>
2018-01-14 07:55:51 +0000

    Fix UX

--- ddx.js
+++ ddx.js
@@ -1,15 +1,15 @@
 
-// For computing derivatives
+	// For computing derivatives
 
 
-function derive(xs, ys){
-	var derivs = [];
-	ys.unshift(ys[0] - (ys[1] - ys[0]));
-	ys.push(ys[ys.length - 1] - (ys[ys.length -1] - ys[ys.length - 2]));
-	// Pad first and last values of our function to make derivs easier
-	for (i = 0; i < xs.length; i++){
-		derivs.push((ys[i+2] - ys[i])/20); 
+	function derive(xs, ys){
+		var derivs = [];
+		ys.unshift(ys[0] - (ys[1] - ys[0]));
+		ys.push(ys[ys.length - 1] - (ys[ys.length -1] - ys[ys.length - 2]));
+		// Pad first and last values of our function to make derivs easier
+		for (i = 0; i < xs.length; i++){
+			derivs.push((ys[i+2] - ys[i])/50); 
+		}
+		return derivs;
 	}
-	return derivs;
-}
 
